89cbe462d504325a816c2f538d7da561948e040f
Huy Ha <huyha2016@gmail.com>
2023-03-06 11:14:22 -0800

    group the files

--- UI.js
+++ UI.js
@@ -37,3 +37,39 @@ class UI {
         items.forEach((item) => this.displayItem(item));
     }
   }
+
+  class Storage {
+    constructor(list) {
+        this.list = list;
+    }
+
+    getStorage() {
+        let items;
+        if(localStorage.getItem(this.list) === null){
+            items = [];
+          } else {
+            items = JSON.parse(localStorage.getItem(this.list));
+          }
+        return items;
+    }
+
+    storeItem(item) {
+        let items = this.getStorage();
+        items.push(item);
+        localStorage.setItem(this.list, JSON.stringify(items));
+      }
+
+    removeItem(itemHtml) {
+        let items = this.getStorage();
+        items.forEach((item, index) => {
+            if(itemHtml.textContent === item) {
+                items.splice(index,1);
+            }
+        });
+        localStorage.setItem(this.list, JSON.stringify(items));
+    }
+
+    clearStorage() {
+        localStorage.setItem(this.list, JSON.stringify([]));
+    }
+}
